5386bcc33e259665dfb72e039acac1b07960a3d8
mrfinker <caalebs@gmail.com>
2022-07-18 09:55:29 +0000

    ____newday

---
+++ HTML_CSS/Youtube/1_projet/main.js
@@ -0,0 +1,8 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen() {
+    myWidth = window.innerWidth;
+
+    document.getElementById("size").innerHTML = "Width : " + myWidth + "px";
+}
